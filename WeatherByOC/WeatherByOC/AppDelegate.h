a5d9f6c0873ecd14d19807e83ddda5bea2003b59
ZukangWang <zukang89@hotmail.com>
2015-07-25 18:56:22 +0800

    代码迁入

---
+++ WeatherByOC/WeatherByOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherByOC
+//
+//  Created by 王祖康 on 15/7/25.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherByOC/WeatherByOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherByOC
+//
+//  Created by 王祖康 on 15/7/25.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
